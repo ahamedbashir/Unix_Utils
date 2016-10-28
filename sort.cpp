eaea3cbf189b70c9fbead380ffd26fd24cbe4654
Bashir Ahamed <bahamed000@citymail.cuny.edu>
2016-10-27 23:31:35 -0400

    need to fix for string with leading whitespace

--- sort.cpp
+++ sort.cpp
@@ -2,9 +2,15 @@
 #include <getopt.h> // to parse long arguments.
 #include <string>
 using std::string;
+#include <cctype>
+using std::tolower;
+using std::toupper;
+#include <algorithm>
+using std::lexicographical_compare;
 #include <iostream>
 using std::cin;
 using std::cout;
+using std::endl;
 #include <set>
 using std::set;
 using std::multiset;
@@ -23,6 +29,21 @@ struct igncaseComp {
 		return (strcasecmp(s1.c_str(),s2.c_str()) < 0);
 	}
 };
+
+/*
+struct icompare_char {
+	bool operator()(char c1, char c2) {
+		return toupper(c1) < toupper(c2);
+	}
+};
+
+// return true if s1 comes before s2
+struct compare {
+	bool operator()(string const& s1, string const& s2) {
+		return lexicographical_compare(s1.begin(), s1.end(),s2.begin(), s2.end(), icompare_char());
+	}
+};*/
+
 /* NOTE: set<string,igncaseComp> S; would declare a set S which
  * does its sorting in a case-insensitive way! */
 
@@ -60,6 +81,56 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* TODO: write me... */
+	
+	set<string, igncaseComp> fuline;
+	set<string, igncaseComp>uline;    //problem when there is leading tab or whitespace
+	multiset<string, igncaseComp> fmline;
+	multiset<string, igncaseComp> mline; //problem when there is leadding tab or white space
+	string line;
+	while ( getline(cin,line) ) {
+		uline.insert(line);
+		fuline.insert(line);
+		mline.insert(line);
+		fmline.insert(line);
+	}
+	
+	if (  descending == 0 && ignorecase == 0 && unique == 0 ) {
+		for (multiset<string,igncaseComp>::iterator i = mline.begin(); i != mline.end(); ++i )
+			cout << *i << endl;
+	}
+	
+	else if (  descending == 1 && ignorecase == 0 && unique == 0 ) {
+		for (multiset<string,igncaseComp>::reverse_iterator i = mline.rbegin(); i != mline.rend(); ++i )
+			cout << *i << endl;
+	}
+
+	else if (  descending == 0 && ignorecase == 1 && unique == 0 ) {
+		for (multiset<string,igncaseComp>::iterator i = fmline.begin(); i != fmline.end(); ++i )
+			cout << *i << endl;
+	}
+	else if (  descending == 1 && ignorecase == 1 && unique == 0 ) {
+		for (multiset<string,igncaseComp>::reverse_iterator i = fmline.rbegin(); i != fmline.rend(); ++i )
+			cout << *i << endl;
+	}
+
+	else if (  descending == 0 && ignorecase == 0 && unique == 1 ) {
+		for (set<string,igncaseComp>::iterator i = uline.begin(); i != uline.end(); ++i )
+			cout << *i << endl;
+	}
+	else if (  descending == 1 && ignorecase == 0 && unique == 1 ) {
+		for (set<string,igncaseComp>::reverse_iterator i = uline.rbegin(); i != uline.rend(); ++i )
+			cout << *i << endl;
+	}
+
+	else if (  descending == 0 && ignorecase == 1 && unique == 1 ) {
+		for (set<string, igncaseComp>::iterator i = fuline.begin(); i != fuline.end(); ++i )
+			cout << *i << endl;
+	}
+	else if (  descending == 1 && ignorecase == 1 && unique == 1 ) {
+		for (set<string, igncaseComp>::reverse_iterator i = fuline.rbegin(); i != fuline.rend(); ++i )
+			cout << *i << endl;
+	}
+
 
 	return 0;
 }
